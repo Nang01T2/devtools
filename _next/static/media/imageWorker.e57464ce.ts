a263deee626fb792374de7965f304af51f9e88f7
Nang01T2 <8766071+Nang01T2@users.noreply.github.com>
2025-12-26 14:23:44 +0000

    Deploying to gh-pages from @ Nang01T2/devtools@f758c662b455608523d3c2c510fde7b9430a7905 🚀

--- _next/static/media/imageWorker.e57464ce.ts
+++ _next/static/media/imageWorker.e57464ce.ts
@@ -1,7 +1,9 @@
 import { expose } from "comlink";
 import { timed } from "../wasm-utils";
 import avifEncode from "@/lib/encoders/avif/avifEncode";
+import webpEncode from "@/lib/encoders/webp/webpEncode";
 import avifDecode from "@/lib/decoders/avif/avifDecode";
+import webpDecode from "@/lib/decoders/webp/webpDecode";
 import mozjpegEncode from "@/lib/encoders/mozJPEG/mozjpegEncode";
 
 const exports = {
@@ -10,10 +12,20 @@ const exports = {
   ): ReturnType<typeof avifEncode> {
     return timed("avifEncode", () => avifEncode(...args));
   },
+  webpEncode(
+    ...args: Parameters<typeof webpEncode>
+  ): ReturnType<typeof webpEncode> {
+    return timed("webpEncode", () => webpEncode(...args));
+  },
   avifDecode(
     ...args: Parameters<typeof avifDecode>
   ): ReturnType<typeof avifDecode> {
     return timed("avifDecode", () => avifDecode(...args));
+  },
+  webpDecode(
+    ...args: Parameters<typeof webpDecode>
+  ): ReturnType<typeof webpDecode> {
+    return timed("webpDecode", () => webpDecode(...args));
   },
   mozjpegEncode(
     ...args: Parameters<typeof mozjpegEncode>
